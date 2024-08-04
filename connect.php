8588cf0e7ae78742b94a98f05b1196463fe6b3d4
Alyssa Paola Pocaan <72067364+alyssapaola@users.noreply.github.com>
2024-08-04 21:14:41 +0800

    Add files via upload

--- connect.php
+++ connect.php
@@ -5,9 +5,6 @@
 	$con = mysqli_connect("localhost", "root", "") or die("Connection Problem" . mysqli_errno($con));
 	$database = mysqli_select_db($con, "db_cel_form") or die("SQL Problem" . mysqli_error($con));
 	
-	//date_default_timezone_set($timezone);
-	date_default_timezone_set("Asia/Manila");
-	$today = date('F j, Y, h:i a');//gmt+8
 	
 	$connect = new PDO("mysql:host=localhost;dbname=db_cel_form", "root", "");
 ?>
